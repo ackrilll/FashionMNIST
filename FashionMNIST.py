c51c650ead52d328477f69be9977381c73c4c46a
Robbie <Robbie@email.com>
2025-04-04 22:57:13 +0900

    모델 및 파라미터 디렉토리 따로 만들었음

--- FashionMNIST.py
+++ FashionMNIST.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+from Models.models import NeuralNet
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torchvision.utils
@@ -12,43 +12,12 @@ import os
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
-#모델 정의
-class NeuralNet(nn.Module):
-    def __init__(self):
-        super(NeuralNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, 3)
-        self.conv2 = nn.Conv2d(6, 16, 3)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
-    def num_flat_features(self, x):
-        size = x.size()[1:]
-        num_features = 1
-        for s in size:
-            num_features *= s
-        return num_features
-
-    def forward(self, x):
-        x = F.max_pool2d(F.leaky_relu(self.conv1(x)), (2, 2))
-        x = F.max_pool2d(F.leaky_relu(self.conv2(x)), 2)
-        x = x.view(-1, self.num_flat_features(x))
-        x = F.leaky_relu(self.fc1(x))
-        x = F.leaky_relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
-
-net = NeuralNet()
-params = list(net.parameters())
-print('모델: ',net)
-print('파라미터 텐서 개수: ',len(params))
-print('conv1 텐서 크기: ',params[0].size())
-
 if __name__ == '__main__':
     multiprocessing.freeze_support() # 윈도우 환경에서 필요
 
     #GPU 설정
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    net = NeuralNet().to(device)
     print('crrent device: ',device)
 
     #데이터 전처리
@@ -68,7 +37,6 @@ if __name__ == '__main__':
     train_loader = DataLoader(trainset,batch_size=128, shuffle = True,num_workers=0)
     test_loader = DataLoader(testset,batch_size=128, shuffle = True,num_workers=0)
 
-
     images, labels = next(iter(train_loader))
     print('image shape: ', images.shape)
     print('label shape: ', labels.shape)
@@ -87,6 +55,7 @@ if __name__ == '__main__':
         9: 'Ankle Boot'
      }
 
+    #훈련 데이터 꺼내서 확인해보기
     figure = plt.figure(figsize=(12, 12))
     cols, rows = 4, 4
     for i in range(1, cols * rows + 1):
@@ -97,19 +66,20 @@ if __name__ == '__main__':
         plt.title(label)
         plt.axis('off')
         plt.imshow(image,cmap = 'gray')
-
     plt.show()
 
-    # 손실함수 및 옵티마이저 설정
+    #손실함수 및 옵티마이저 설정
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
     '''
     # 모델 훈련
-    for epoch in range(10):
+    losses = []
+    epochs = []
+    for epoch in range(50):
         running_loss = 0.0
         for i, data in enumerate(train_loader, 0):
             inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
             optimizer.zero_grad()
             outputs = net(inputs)
             loss = criterion(outputs, labels)
@@ -118,15 +88,24 @@ if __name__ == '__main__':
             running_loss += loss.item()
             if i % 100 == 99:
                 print(' Epoch: {}, Iter: {}, Loss: {}'.format(
-                    epoch + 1, i + 1, running_loss / 2000))
+                    epoch + 1, i + 1, running_loss))
+                losses.append(running_loss)
+                epochs.append(epoch)
                 running_loss = 0.0
     
+    # 로스 그래프
+    plt.plot(epochs,losses)
+    plt.title('loss curve')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.show()
+    
     #모델 저장
-    PATH = '.\\fashion_mnist2.pth'
+    PATH = '.\\Parameters\\fashion_mnist2.pth'
     torch.save(net.state_dict(),PATH)
     '''
     #모델 로드
-    PATH = '.\\fashion_mnist2.pth'
+    PATH = '.\\Parameters\\fashion_mnist2.pth'
     net = NeuralNet().to(device)
     net.load_state_dict(torch.load(PATH))
 
